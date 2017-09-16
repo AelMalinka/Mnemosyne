b074d3fe7f2329c495e4b8f1472eb8b49b5b2456
Michael Thomas (malinka) <malinka@entropy-development.com>
2017-09-15 21:22:04 -0400

    test application

---
+++ test/application/basic.cc
@@ -0,0 +1,16 @@
+/*	Copyright 2017 (c) Michael Thomas (malinka) <malinka@entropy-development.com>
+	Distributed under the terms of the GNU Affero General Public License v3
+*/
+
+#include <gtest/gtest.h>
+#include "Application.hh"
+
+using namespace std;
+using namespace testing;
+using namespace Entropy::Mnemosyne;
+
+namespace {
+	TEST(Application, Create) {
+		Application app;
+	}
+}
