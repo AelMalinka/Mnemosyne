7fbc433e5dc545ab0bbe7d651c74ff3d2cb58fc8
Michael Thomas (malinka) <malinka@entropy-development.com>
2017-09-30 17:50:08 -0400

    add Mode to Application object

--- src/Application.cc
+++ src/Application.cc
@@ -16,8 +16,17 @@
 #	endif
 #endif
 
-using namespace Entropy;
+#include "Events.hh"
+
+using namespace Entropy::Mnemosyne;
 using namespace std;
+
+namespace Tethys = Entropy::Tethys;
+namespace Aoede = Entropy::Aoede;
+namespace Theia = Entropy::Theia;
+
+using Entropy::Severity;
+using Entropy::Theia::PolymorphicList;
 
 #if defined(HAVE_CXX_FS) && !defined(HAVE_CXX_FS_EXPERIMENTAL)
 	using namespace std::filesystem;
@@ -25,21 +34,61 @@ using namespace std;
 	using namespace std::experimental::filesystem::v1;
 #endif
 
-Mnemosyne::Application::Application() :
+Application::Application() :
 	Tethys::Application(),
 	Aoede::Application(),
-	Theia::Application()
+	Theia::Application(),
+	_paths(),
+	_modes(),
+	_current(_modes.end())
 {}
 
-Mnemosyne::Application::Application(const int argc, char *argv[]) :
+Application::Application(const int argc, char *argv[]) :
 	Tethys::Application(argc, argv),
 	Aoede::Application(argc, argv),
-	Theia::Application(argc, argv)
+	Theia::Application(argc, argv),
+	_paths(),
+	_modes(),
+	_current(_modes.end())
 {}
 
-Mnemosyne::Application::~Application() = default;
+Application::~Application() = default;
 
-void Mnemosyne::Application::addSearchPath(const string &path)
+void Application::operator () ()
+{
+	if(_current == _modes.end())
+		ENTROPY_THROW(NoMode());
+
+	Theia::Application::operator () ();
+}
+
+Mode &Application::getMode()
+{
+	if(_current == _modes.end())
+		ENTROPY_THROW(NoMode());
+
+	return *_current;
+}
+
+void Application::setMode(const PolymorphicList<Mode>::iterator &i)
+{
+	_current = i;
+
+	Events::ModeChange ev(*this, getMode());
+	onEvent(ev);
+}
+
+void Application::addMode(const shared_ptr<Mode> &mode)
+{
+	_modes.push_back(mode);
+}
+
+void Application::onEvent(const Entropy::Event &ev)
+{
+	_current->onEvent(ev);
+}
+
+void Application::addSearchPath(const string &path)
 {
 #	ifndef HAVE_CXX_FS
 		ENTROPY_THROW(Exception("Search Paths unsuported"));
@@ -48,14 +97,14 @@ void Mnemosyne::Application::addSearchPath(const string &path)
 	_paths.push_front(path);
 }
 
-string Mnemosyne::Application::findFullPath(const string &file) const
+string Application::findFullPath(const string &file) const
 {
-	ENTROPY_LOG(Mnemosyne::Log, Severity::Debug) << "Loading " << file;
+	ENTROPY_LOG(Log, Severity::Debug) << "Loading " << file;
 
 #	ifdef HAVE_CXX_FS
 		for(auto &i : _paths) {
 			path p(i + "/"s + file);
-			ENTROPY_LOG(Mnemosyne::Log, Severity::Debug) << "Trying " << i << " at " << p;
+			ENTROPY_LOG(Log, Severity::Debug) << "Trying " << i << " at " << p;
 
 			if(exists(p)) {
 				return p;
--- src/Application.hh
+++ src/Application.hh
@@ -8,12 +8,16 @@
 #	include "Exception.hh"
 #	include <Entropy/Aoede/Application.hh>
 #	include <Entropy/Theia/Application.hh>
-#	include <list>
+#	include <Entropy/Theia/Utility/PolymorphicList.hh>
+
+#	include "Mode.hh"
 
 	namespace Entropy
 	{
 		namespace Mnemosyne
 		{
+			ENTROPY_EXCEPTION(NoMode, "No Game Mode Loaded", Exception);
+
 			class Application :
 				public virtual Aoede::Application,
 				public virtual Theia::Application
@@ -22,13 +26,21 @@
 					Application();
 					Application(const int, char *[]);
 					virtual ~Application();
+					virtual void operator () ();
+					virtual Mode &getMode();
+					virtual void setMode(const Theia::PolymorphicList<Mode>::iterator &);
+					virtual void addMode(const std::shared_ptr<Mode> &);
 					template<typename F>
 					auto load(const std::string &, F) const;
 					virtual void addSearchPath(const std::string &);
 				protected:
+					virtual void onEvent(const Entropy::Event &);
+				protected:
 					virtual std::string findFullPath(const std::string &) const;
 				private:
 					std::list<std::string> _paths;
+					Theia::PolymorphicList<Mode> _modes;
+					Theia::PolymorphicList<Mode>::iterator _current;
 			};
 		}
 	}
